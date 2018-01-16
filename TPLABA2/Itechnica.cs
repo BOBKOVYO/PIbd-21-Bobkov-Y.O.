5cd64d4757ccd822a6598ceb830982f8a444b0bf
PIBD-21BOBKOVYO <32008797+PIBD-21BOBKOVYO@users.noreply.github.com>
2018-01-16 20:01:05 +0300

    Add files via upload

---
+++ TPLABA2/Itechnica.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TPLABA2
+{
+    public interface Itechnica
+    {
+        void moveSamolet(Graphics g);
+        void drawSamolet(Graphics g);
+        void SetPosition(int x, int y);
+        void loadVzletMass(int count);
+        int getVzletMass();
+    }
+}
---
+++ TPLABA2/letniitransport.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TPLABA2
+{
+    public abstract class letniitransport : Itechnica
+    {
+        protected float startPosX;
+        protected float startPosY;
+        protected int countVzletMass;
+        public virtual int MaxcountVzletMass { protected set; get; }
+        public virtual int MaxSpeed { protected set; get; }
+        public Color ColorBody { protected set; get; }
+        public virtual double Weight { protected set; get; }
+        public abstract void moveSamolet(Graphics g);
+        public abstract void drawSamolet(Graphics g);
+        public void SetPosition(int x, int y)
+        {
+            startPosX = x;
+            startPosY = y;
+        }
+        public void loadVzletMass(int count)
+        {
+            if (countVzletMass + count < MaxcountVzletMass)
+            {
+                countVzletMass += count;
+            }
+        }
+        public int getVzletMass()
+        {
+            int count = countVzletMass;
+            countVzletMass = 0;
+            return count;
+        }
+    }
+}
